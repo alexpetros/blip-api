cc0ba9781eadb2dc0b39b47b7ce82fe2f5b34963
Alexander Petros <apetros15@gmail.com>
2018-05-16 01:56:21 -0400

    switched output to json

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    defaultResponse = {
+        'text': 'Hello World!',
+    }
+
+    return jsonify(defaultResponse)
+
 
 if __name__ == '__main__':
     app.run()
